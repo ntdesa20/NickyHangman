76a79faec49fa861f0cb83cfd89a0e3e7ff34f1a
ntdesa20 <60932543+ntdesa20@users.noreply.github.com>
2020-03-02 11:29:20 -0500

    Revert "Update HangMan.py"
    This reverts commit 92c143f0a938b71fff5858f673ef277ec33b9a3d.

--- HangMan.py
+++ HangMan.py
@@ -8,39 +8,3 @@ def checkLetter():
         lives = lives - 1
         getLetter() 
         
-word = ("Kyle")
-wordList = list(word.lower())
-updatedSpaces = []
-wordLen = len(word)
-lives = 5
-letter = " "
-
-for i in range (0, int(wordLen)):
-    updatedSpaces.append("_")
-    
-def getLetter():
-    global letter
-    letter = raw_input ("Guess a letter   ")
-
-def check():
-    global updatedSpaces
-    global lives
-    global letter
-    for x in range(0, int(wordLen)):
-        if letter == wordList[x]:
-            updatedSpaces[x] = wordList[x]
-            print(updatedSpaces)
-            getLetter()
-        else:
-            lives = -1
-            if lives != 0:
-                print(updatedSpaces)
-                getLetter()
-            else:
-                print("Game Over")
-                
-def game():
-    getLetter()
-    check()
-    
-game()              
